48821f996aa3cc224715887c0c0256244c3d7256
adrianadhari <adrianadhari@protonmail.com>
2024-08-13 20:06:04 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
         </div>
       </div>
 
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 py-20">
         <div className="text-center">
           <h2 className="text-3xl font-bold mb-3">Assalamualaikum Wr. Wb.</h2>
           <p className="mb-3">
